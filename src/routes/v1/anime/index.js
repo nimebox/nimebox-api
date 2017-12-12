820c78de11f5db881d91390153358debde78452d
xdk78 <xdk78@interia.pl>
2017-12-12 17:05:35 +0100

    Add anime eps and players from senpai

--- src/routes/v1/anime/index.js
+++ src/routes/v1/anime/index.js
@@ -4,5 +4,21 @@ module.exports = (fastify, opts, next) => {
     reply.header('Content-Type', 'application/json').code(200)
     senpai.animeList().then(el => reply.send(el)).catch(err => reply.send(err))
   })
+  fastify.post('/anime/episodes', opts, (req, reply) => {
+    reply.header('Content-Type', 'application/json').code(200)
+    if (req.body === undefined || !req.body.url) {
+      reply.send({error: 'Missing body param'})
+    } else {
+      senpai.episodes(req.body.url).then(el => reply.send(el)).catch(err => reply.send(err))
+    }
+  })
+  fastify.post('/anime/episodes/players', opts, (req, reply) => {
+    reply.header('Content-Type', 'application/json').code(200)
+    if (req.body === undefined || !req.body.url) {
+      reply.send({error: 'Missing body param'})
+    } else {
+      senpai.players(req.body.url).then(el => reply.send(el)).catch(err => reply.send(err))
+    }
+  })
   next()
 }
--- src/scraper/senpai.js
+++ src/scraper/senpai.js
@@ -38,6 +38,62 @@ const animeList = async () => {
   })
 }
 
+const episodes = async (url) => {
+  const response = await api.get(url)
+  return new Promise((resolve, reject) => {
+    x(response.data, {
+      number: ['div[class="collection row anime-col"] > a[class="collection-item anime-item"] > div[class="anime-number"] > h5'],
+      url: ['div[class="collection row anime-col"] > a[class="collection-item anime-item"]@href'],
+      description: ['div[class="collection row anime-col"] > a[class="collection-item anime-item"] > div[class="anime-desc"] > span[class="grey-text text-lighten-1"]']
+    })((err, obj) => {
+      if (err) {
+        reject(err)
+      }
+
+      const list = _.compact(obj.number).map((el, i) => {
+        return ({
+          number: el,
+          url: BASE_URL + obj.url[i],
+          description: obj.description[i]
+        })
+      })
+      resolve(list)
+    })
+  })
+}
+
+const players = async (url) => {
+  const response = await api.get(url)
+  return new Promise((resolve, reject) => {
+    x(response.data, {
+      host: ['div[class="container"] > ul[class="tabs"] > li[class="tab"] > a'],
+      player1: 'div[class="container"] > div[id="pl1"] > div[class="video-container"] > iframe@src',
+      player2: 'div[class="container"] > div[id="pl2"] > div[class="video-container"] > iframe@src',
+      player3: 'div[class="container"] > div[id="pl3"] > div[class="video-container"] > iframe@src'
+    })((err, obj) => {
+      if (err) {
+        reject(err)
+      }
+
+      const pls = [
+        obj.player1,
+        obj.player2,
+        obj.player3
+      ]
+
+      const list = pls.map((el, i) => {
+        return ({
+          host: obj.host[i],
+          player: el
+        })
+      })
+      resolve(list)
+    })
+  })
+}
+
 module.exports = {
-  animeList
+  animeList,
+  episodes,
+  players
 }
