08f25376abb90899f6c5ad73ffc1ebceb248c9e5
xdk78 <xdk78@interia.pl>
2018-07-29 18:48:38 +0200

    feat: add config based on env vars

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import dotenv from 'dotenv-safe'
+let envConfig = {}
+if (process.env.NODE_ENV === 'production') {
+  envConfig = { path: './.env.production' }
+} else if (process.env.NODE_ENV === 'test') {
+  envConfig = { path: './.env.test' }
+} else {
+  envConfig = { path: './.env' }
+}
+dotenv.config(Object.assign({}, { allowEmptyValues: true, example: './.env.example' }, envConfig))
 const fastify = require('fastify')()
 const PORT = process.env.PORT || 5000
-const keys = new Set(['c0366e6f04436200b9998419134e4c3216b08daf'])
+const HOST = process.env.HOST || '0.0.0.0'
+const keys = new Set([process.env.API_BEARER_SECRET_TOKEN])
 fastify.register(require('fastify-helmet'))
 fastify.register(require('fastify-bearer-auth'), {keys}, (err) => {
   if (err) {
@@ -21,7 +32,7 @@ fastify.register(require('./routes/v1/'), { prefix: '/v1' })
 fastify.register(require('./routes/v1/anime'), { prefix: '/v1' })
 fastify.register(require('./routes/v1/news'), { prefix: '/v1' })
 
-fastify.listen(PORT, '0.0.0.0', (err) => {
+fastify.listen(PORT, HOST, (err) => {
   if (err) throw err
   console.log(`Nimebox Middleware is listening on ${fastify.server.address().port}`)
 })
