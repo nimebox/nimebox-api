b541c7bc240b2077fc2105a2126ec1ba057ee0c1
xdk78 <xdk78888@gmail.com>
2019-04-14 20:28:09 +0200

    feat: re-add anime24 news source

--- src/routes/v1/news/index.ts
+++ src/routes/v1/news/index.ts
@@ -1,13 +1,16 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import { ServerResponse, IncomingMessage } from 'http'
+import Anime24 from '../../../scrapers/Anime24'
+
+const animeNews = new Anime24()
 
 export default async (fastify: FastifyInstance, opts) => {
   fastify.get('/news', opts, async (req: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>) => {
     reply.header('Content-Type', 'application/json').code(200)
     try {
-      const res = {}
+      const res = await animeNews.getNews()
       return {
-        serviceId: 'animeNews.serviceId',
+        serviceId: animeNews.serviceId,
         data: res
       }
     } catch (err) {
