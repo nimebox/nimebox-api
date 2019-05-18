2d519948127cce475f261f1b10f75199ece66b26
xdk78 <xdk78888@gmail.com>
2019-05-18 16:34:57 +0200

    fix: anime24 scraper

--- src/scrapers/Anime24.ts
+++ src/scrapers/Anime24.ts
@@ -3,42 +3,37 @@ import BaseScraper, { IBaseScraperResponse } from './BaseScraper'
 export default class Anime24 extends BaseScraper {
   constructor() {
     super()
-    this.baseUrl = 'http://anime24.pl'
+    this.baseUrl = 'https://anime24.pl'
     this.serviceId = 'anime24'
   }
 
-  public async getNews(): Promise<IBaseScraperResponse[]> {
+  public async getNews(): Promise<IBaseScraperResponse[] | any> {
     try {
       const { doc } = await this.api('news')
 
       const obj = {
         title: [
           ...doc.querySelectorAll(
-            'div[class="news-module"] > div[class="news"] > div[class="news-info-bar"] > div[class="news-title-bar float-left"] > div[class="news-title"] > a:nth-of-type(2)'
+            'div[class="cell small-12 medium-9"] > h5 > a > span[class="field field--name-title field--type-string field--label-hidden"]'
           )
         ],
-        url: [
-          ...doc.querySelectorAll(
-            'div[class="news-module"] > div[class="news"] > div[class="news-info-bar"] > div[class="news-title-bar float-left"] > div[class="news-title"] > a[href]'
-          )
-        ],
+        url: [...doc.querySelectorAll('div[class="cell small-12 medium-9"] > h5 > a[href]')],
         date: [
           ...doc.querySelectorAll(
-            'div[class="news-module"] > div[class="news"] > div[class="news-info-bar"] > div[class="news-title-bar float-left"] > div[class="news-date"]'
+            'div[class="cell small-12 medium-9"] > div[class="teaser-category"] > span[class="teaser-date"]'
           )
         ],
-        description: [...doc.querySelectorAll('div[class="news-module"] > div[class="news"] > div[class="news-desc"]')],
+        description: [...doc.querySelectorAll('div[class="cell small-12 medium-9"] > div[class="node__content"]')],
         image: [
-          ...doc.querySelectorAll(
-            'div[class="news-module"] > div[class="news"] > div[class="news-category-image"] > a > img[src]'
-          )
+          ...doc.querySelectorAll('div[class="cell small-12 medium-3"] > figure[class="teaser-image"] > a > img[src]')
         ]
       }
+
       return obj.title.map((el, i) => {
         return {
           title: el.textContent.trim(),
-          url: obj.url[i].getAttribute('href'),
-          date: obj.date[i].textContent,
+          url: `${this.baseUrl}/${obj.url[i].getAttribute('href')}`,
+          date: obj.date[i].textContent.slice(1, -1),
           description: obj.description[i].textContent.trim(),
           image: obj.image[i].getAttribute('src')
         }
