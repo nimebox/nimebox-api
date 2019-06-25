b285a2b567a008d32a89e23d4771766fbd1e4f31
rnickson <m@mickson.me>
2019-06-26 01:25:49 +0200

    test: HorribleSubs

---
+++ tests/horriblesubs.spec.ts
@@ -0,0 +1,29 @@
+jest.setTimeout(100000)
+
+import HorribleSubsScraper from '../src/scrapers/HorribleSubs'
+const horrible = new HorribleSubsScraper()
+const animeTitle = 'Sewayaki Kitsune no Senko-san'
+
+describe('HorribleSubs scraper', () => {
+  it('should respond with anime list', async () => {
+    const animeList = await horrible.getAnimeList()
+    
+    expect(Array.isArray(animeList)).toBeTruthy()
+    expect(animeList).not.toBeNull()
+    animeList.forEach((el) => {
+      expect(el.title).toBeDefined()
+      expect(el.url).toBeDefined()
+    })
+  })
+
+  it('should respond with one anime', async () => {
+    const anime = await horrible.getAnime(animeTitle)
+    
+    expect(Array.isArray(anime)).toBeTruthy()
+    expect(anime).not.toBeNull()
+    anime.forEach((el) => {
+      expect(el.title).toBeDefined()
+      expect(el.url).toBeDefined()
+    })
+  })
+})
